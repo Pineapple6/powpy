cd64f5b9f39a76a6917266efc9eca1c88a195c2a
Piña <adanielpincab@gmail.com>
2018-06-15 12:52:45 +0200

    Update setup.py
    Added the github URL, which I could only get creating this repository. Ironical.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A simple proof of work using MD5 hashes",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/Pineapple6/powpy",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
